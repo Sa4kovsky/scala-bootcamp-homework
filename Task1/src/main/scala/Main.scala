2c924c9dd4edc09fd75c0aefd8fcc7080eab9634
Evgeny Sachkovsky <sa4kovsky@gmail.com>
2021-02-01 10:45:38 +0300

    Task 1 => fixed the method lcm

--- Task1/src/main/scala/Main.scala
+++ Task1/src/main/scala/Main.scala
@@ -1,9 +1,12 @@
 object Main {
 
   def gcd(a: Int, b: Int): Int = (a, b) match {
-    case (aa, 0) => aa
-    case (aa, bb) => gcd(bb, aa % bb)
+    case (a, 0) => a
+    case (a, b) => gcd(b, a % b)
   }
 
-  def lcm(a: Int, b: Int): Int = math.abs(a * b) / gcd(a, b)
+  def lcm(a: Int, b: Int): Option[Int] = (a,b) match {
+    case (0, 0) => None
+    case _     => Some(math.abs(a * b) / gcd(a, b))
+  }
 }
--- Task1/src/test/scala/MainSpec.scala
+++ Task1/src/test/scala/MainSpec.scala
@@ -24,14 +24,18 @@ class MainSpec extends AnyFlatSpec  {
   }
 
   "lcm" should "give 0 for 0 and 12" in {
-    lcm(0, 12) shouldEqual 0
+    lcm(0, 12) shouldEqual Some(0)
   }
 
   "lcm" should "give 36 for 9 and 12" in {
-    lcm(9, 12) shouldEqual 36
+    lcm(9, 12) shouldEqual Some(36)
   }
 
   "lcm" should "give 900 for 50 and 180" in {
-    lcm(50, 180) shouldEqual 900
+    lcm(50, 180) shouldEqual Some(900)
+  }
+
+  "lcm" should "give 0 for 0 and None" in {
+    lcm(0, 0) shouldEqual None
   }
 }
