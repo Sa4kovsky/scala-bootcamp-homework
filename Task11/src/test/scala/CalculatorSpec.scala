59be7df1d280397edecd042a02a5e0ff6c4cfdd6
Evgeny Sachkovsky <sa4kovsky@gmail.com>
2021-03-18 15:07:51 +0300

    Task 11 -> update title test

--- Task11/src/test/scala/CalculatorSpec.scala
+++ Task11/src/test/scala/CalculatorSpec.scala
@@ -8,64 +8,89 @@ class CalculatorSpec extends AnyFunSuite {
     assert(Left("Error: Input is not correct") ==  testCalc.calculate(Calculator()))
   }
 
-  test("work correctly when the addition 1 + 0 is 1") {
-    val testCalc = Calculator()
-    assert(Right(1.0) ==  testCalc.calculate(Calculator(1, 0).plus))
-  }
-
-  test("work correctly when the addition -8 + 2 is -6") {
-    val testCalc = Calculator()
-    assert(Right(-6.0) ==  testCalc.calculate(Calculator(-8, 2).plus))
-  }
-
-  test("work correctly when the addition 10 + 2 is 12") {
+  test("test - addition of two positive numbers") {
     val testCalc = Calculator()
     assert(Right(12.0) ==  testCalc.calculate(Calculator(10, 2).plus))
   }
 
-  test("work correctly when subtracting 10 - 2 is 8") {
+  test("test - addition of a positive and negative number") {
+    val testCalc = Calculator()
+    assert(Right(-6.0) ==  testCalc.calculate(Calculator(-8, 2).plus))
+  }
+
+  test("test - addition of two negative numbers") {
+    val testCalc = Calculator()
+    assert(Right(-10.0) ==  testCalc.calculate(Calculator(-8, -2).plus))
+  }
+
+  test("test - addition of a number and its inverse gives zero") {
+    val testCalc = Calculator()
+    assert(Right(0.0) ==  testCalc.calculate(Calculator(-1, 1).plus))
+  }
+
+  test("test - difference of two positive numbers") {
     val testCalc = Calculator()
     assert(Right(8.0) ==  testCalc.calculate(Calculator(10, 2).minus))
   }
 
-  test("work correctly when subtracting -2 - 2 is -4") {
+  test("test - difference of positive and negative numbers") {
     val testCalc = Calculator()
     assert(Right(-4.0) ==  testCalc.calculate(Calculator(-2, 2).minus))
   }
 
-  test("work correctly when subtracting -2 - (-2) is 0") {
+  test("test - difference of two negative numbers") {
     val testCalc = Calculator()
     assert(Right(0.0) ==  testCalc.calculate(Calculator(-2, -2).minus))
   }
 
-  test("works correctly when the division 10 / 2 is 5") {
+  test("test - division of two positive gives a positive") {
     val testCalc = Calculator()
     assert(Right(5.0) ==  testCalc.calculate(Calculator(10, 2).divide))
   }
 
-  test("works correctly when the division 10 / (-2) is -5") {
+  test("test - division of two negative numbers") {
+    val testCalc = Calculator()
+    assert(Right(2.0) ==  testCalc.calculate(Calculator(-4, -2).divide))
+  }
+
+  test("test - dividing a negative number by its own gives one") {
+    val testCalc = Calculator()
+    assert(Right(1.0) ==  testCalc.calculate(Calculator(-4, -4).divide))
+  }
+
+  test("test - division of positive and negative gives a negative") {
     val testCalc = Calculator()
     assert(Right(-5.0) ==  testCalc.calculate(Calculator(10, -2).divide))
   }
 
-  test("works correctly when the division 0 / 2 is 0") {
+  test("test - dividing zero by a number") {
     val testCalc = Calculator()
     assert(Right(0.0) ==  testCalc.calculate(Calculator(0, 2).divide))
   }
 
-  test("outputs an error when dividing by 0") {
+  test("test - dividing a number by zero gives an error") {
     val testCalc = Calculator()
     assert(Left("Error: You can't divide by 0") ==  testCalc.calculate(Calculator(2, 0).divide))
   }
 
-  test("work correctly when multiplying 2 * 0 is 0") {
+  test("test - multiplying a number by 0 gives 0") {
     val testCalc = Calculator()
     assert(Right(0.0) ==  testCalc.calculate(Calculator(2, 0).times))
   }
 
-  test("work correctly when multiplying 2 * 10 is 20") {
+  test("test - multiplying of two positive numbers") {
     val testCalc = Calculator()
     assert(Right(20.0) ==  testCalc.calculate(Calculator(2, 10).times))
+  }
+
+  test("test - multiplying positive and negative numbers gives a negative") {
+    val testCalc = Calculator()
+    assert(Right(-20.0) ==  testCalc.calculate(Calculator(2, -10).times))
+  }
+
+  test("test - multiplying two negative numbers gives a positive") {
+    val testCalc = Calculator()
+    assert(Right(20.0) ==  testCalc.calculate(Calculator(-2, -10).times))
   }
 
 }
