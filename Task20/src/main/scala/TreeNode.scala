c1f8301128b1d6b69a432166321dcdf88cb517fa
Evgeny Sachkovsky <Sa4kovsky@gmail.com>
2021-05-07 12:29:38 +0300

    Task 20 -> create ActorSystem

--- Task20/src/main/scala/TreeNode.scala
+++ Task20/src/main/scala/TreeNode.scala
@@ -1,40 +1,68 @@
-import Operation.{Contains, Insert}
+import OperationTree._
 import TreeNode._
+import TypeOfReply.{ContainsResult, TheEnd}
+import akka.actor.{Actor, ActorRef, Props}
 
-import akka.actor.{Actor, Props}
-import akka.cluster.pubsub.DistributedPubSubMediator.Remove
+import javax.swing.text.Position
+import scala.util.control.TailCalls.TailRec
+
+trait Position
+case object Left extends Position
+case object Right extends Position
 
 object TreeNode {
-  case object Left
-  case object Right
+  import Position._
 
-  def props(elem: Int, initiallyRemoved: Boolean): Props = Props[TreeNode](elem, initiallyRemoved)
+  def props(elem: Int, initiallyRemoved: Boolean): Props = Props(new TreeNode(elem, initiallyRemoved))
 }
 
 final class TreeNode(val elem: Int, initiallyRemoved: Boolean) extends Actor {
 
+  var state = Map[Position, ActorRef]()
+  var removed = initiallyRemoved
+
   override def receive: Receive = {
     case insert: Insert => doInsert(insert)
     case contains: Contains => doContains(contains)
-    case remove: Remove => doRemove(remove)
+    case remove: Delete => doRemove(remove)
   }
+
+  def selectPosition(newElem: Int): Option[ActorRef] = if (newElem < elem) state.get(Left) else state.get(Right)
 
   def doInsert(ins: Insert) =
     if(elem > ins.elem)
       {
-        ???
+       state.get(Left).fold {
+          state = state + (Left -> context.actorOf(props(ins.elem, initiallyRemoved = false)))
+        }(_ ! ins)
+        sender() ! TheEnd(ins.id)
       }
     else if(elem < ins.elem)
       {
-        ???
+        state.get(Right).fold {
+          state = state + (Right -> context.actorOf(props(ins.elem, initiallyRemoved = false)))
+        }(_ ! ins)
+        sender() ! TheEnd(ins.id)
       }
     else
       {
-        ???
+        removed = false
+        sender() ! TheEnd(ins.id)
       }
 
-  def doContains(cont: Contains) = ???
+  def doContains(cont: Contains) = {
+    val position: Option[ActorRef] = selectPosition(cont.elem)
+    if (cont.elem == elem && !removed) sender() ! ContainsResult(cont.id, result = true)
+    else position.fold(sender() ! ContainsResult(cont.id, result = false))(_ ! cont)
+  }
 
-  def doRemove(rem: Remove) = ???
+  def doRemove(rem: Delete) = {
+    val position: Option[ActorRef] = selectPosition(rem.elem)
+    if (rem.elem == elem) {
+      removed = true
+      rem.requester ! ContainsResult(rem.id, removed)
+    } else
+      position.fold(rem.requester ! TheEnd(rem.id))(_ ! rem)
+  }
 
 }
--- Task20/src/main/scala/TreeSet.scala
+++ Task20/src/main/scala/TreeSet.scala
@@ -1,19 +1,46 @@
-import akka.actor.{Actor, ActorRef}
+import akka.actor.{Actor, ActorRef, ActorSystem, Props}
+import akka.cluster.pubsub.DistributedPubSubMediator.Remove
 
-import Operation._
+import scala.util.Random
+import scala.util.chaining.scalaUtilChainingOps
 
-trait Operation
-object Operation{
-  final case class Insert(requester: ActorRef, id: Int, elem: Int) extends Operation
-  final case class Contains(requester: ActorRef, id: Int, elem: Int) extends Operation
-  final case class Remove(requester: ActorRef, id: Int, elem: Int) extends Operation
+sealed  trait OperationTree
+object OperationTree{
+  final case class Insert(requester: ActorRef, id: Int, elem: Int) extends OperationTree
+  final case class Contains(requester: ActorRef, id: Int, elem: Int) extends OperationTree
+  final case class Delete(requester: ActorRef, id: Int, elem: Int) extends OperationTree
 }
 
-object TreeSet extends Actor{
+sealed trait TypeOfReply
+object TypeOfReply {
+  final case class ContainsResult(id: Int, result: Boolean) extends TypeOfReply
+  final case class TheEnd(id: Int) extends TypeOfReply
+}
 
-  private def createRoot: ActorRef = context.actorOf(TreeNode.props(20, initiallyRemoved = true))
+object  ActorModel extends App {
 
-  override def receive: Receive = {
-    case message: Operation => createRoot ! message
+  final class  TreeSet extends Actor {
+
+    import OperationTree._
+    import TypeOfReply._
+
+    private def createRoot: ActorRef = context.actorOf(TreeNode.props(10, initiallyRemoved = true))
+
+    override def receive: Receive = {
+      case "Start" =>
+        (1 to 5).map(i => Insert(createRoot, i, Random.between(5, 15))).foreach { insert =>
+          println(s"Send: $insert")
+          createRoot ! insert
+        }
+
+      case TheEnd(id) =>
+        println(s"Operation with id = $id finished")
+      case ContainsResult(id, result) =>
+        println(s"Contains result with id = $id is $result")
+    }
   }
+
+  val treeActorSystem: ActorSystem = ActorSystem("tree-actor-system")
+  val mainRef: ActorRef = treeActorSystem.actorOf(Props[TreeSet](), "main")
+  mainRef ! "Start"
 }
